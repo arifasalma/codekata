4445efbc193d5f81e4a6e16ab3a5e3617f499c47
arifasalma <47441485+arifasalma@users.noreply.github.com>
2019-02-08 14:51:55 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,6 @@
-b=str(raw_input())
-if(b.isalpha()):
-   if(b=="a" or b=="e" or b=="i" or b=="o" or b=="u" ):
+d=str(raw_input())
+if(d.isalpha()):
+   if(d=="a" or d=="e" or d=="i" or d=="o" or d=="u" ):
         print("Vowel")
    else:
         print("consonant")
