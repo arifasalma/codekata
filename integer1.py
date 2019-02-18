5097c0e5424ade5c1415595fde5e54f2dae40a24
arifasalma <47441485+arifasalma@users.noreply.github.com>
2019-02-18 21:37:12 +0530

    Update integer1.py

--- integer1.py
+++ integer1.py
@@ -1,6 +1,8 @@
-n=int(input("Enter number:"))
-count=0
-while(n>0):
-    count=count+1
-    n=n//10
-print("The number of digits in the number are:",count)
+count = 0 
+number = int(input()) 
+ 
+while (number > 0): 
+  number = number//10 
+  count = count + 1 
+ 
+print (count) 
