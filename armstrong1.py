86005fe65ce91f9315ac7c36b949cff05d81d707
arifasalma <47441485+arifasalma@users.noreply.github.com>
2019-02-19 15:22:58 +0530

    Update armstrong1.py

--- armstrong1.py
+++ armstrong1.py
@@ -1,12 +1,16 @@
-lower=360
-upper=380
-for num in range(lower, upper + 1):
-   order = len(str(num))
-   sum = 0
-   temp = num
-   while temp > 0:
+inp=raw_input().split()
+lower =int(inp[0])
+upper= int(inp[1])
+for num in range(lower+1,upper):
+   order= len(str(num))
+   sum=0
+   temp=num
+   while temp >0:
        digit = temp % 10
        sum += digit ** order
        temp //= 10
    if num == sum:
        print(num)
+       
+       
+ 
