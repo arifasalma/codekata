21baf583885c8c5bf1e83a25a42b7c4eea614ee0
arifasalma <47441485+arifasalma@users.noreply.github.com>
2019-02-19 12:25:34 +0530

    Create primeintervals.py

---
+++ primeintervals.py
@@ -0,0 +1,11 @@
+inp=raw_input().split()
+lower = int(inp[0])
+upper= int(inp[1])
+
+for num in range(lower+1,upper):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
