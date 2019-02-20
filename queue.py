7cd0899e0654d8f51be201751813cc8c594331ba
arifasalma <47441485+arifasalma@users.noreply.github.com>
2019-02-20 12:24:22 +0530

    Update queue.py

--- queue.py
+++ queue.py
@@ -1,4 +1,4 @@
-print("stack implementation")
+print("queue data structure")
 queue=[]
 while True:
     print("what function do you want to perform? \n 1.insert an element,2.remove an element,3.check the size of a stach,4.emptiness of stack.5.exit")
